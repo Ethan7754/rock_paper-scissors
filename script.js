67c2269ae19e61e65064b7984d814676f0652e92
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-01-02 19:43:37 -0700

    Complete getHumanChoice function

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function getComputerChoice() {
 function getHumanChoice() {
   let humanPick = window.prompt("What's your pick?");
   if (humanPick.toLowerCase() === "rock") {
-    return "You picked " + humanPIck;
+    return "You picked " + humanPick.toLowerCase();
   } else if (humanPick.toLowerCase() === "paper") {
-    return "You picked " + humanPIck;
+    return "You picked " + humanPick.toLowerCase();
   } else if (humanPick.toLowerCase() === "scissors")  {
-    return "You picked " + humanPIck;
+    return "You picked " + humanPick.toLowerCase();
   } else {
     return "Please enter a valid choice";
   };

51eff8c7cd35d6444cfb78c3546fe256ee33828f
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-03-03 11:29:33 -0700

    Display scores and ask if user wants to play again, when game is complete

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ rockButton.addEventListener("click", (e) => {
   let comp = getComputerChoice();
   playRound(human, comp);
 });
+
 const paperButton = document.getElementById("paper");
 paperButton.addEventListener("click", (e) => {
   let human = getHumanChoice(e.target.innerText);
   let comp = getComputerChoice();
   playRound(human, comp);
 });
+
 const scissorsButton = document.getElementById("scissors");
 scissorsButton.addEventListener("click", (e) => {
  let human = getHumanChoice(e.target.innerText);
  let comp = getComputerChoice();
  playRound(human, comp);
 });
+
 const humanResultContainer = document.getElementById("human-result");
 const gameResultContainer = document.getElementById("round-and-game-result");
 const computerResultContainer = document.getElementById("computer-result");
+
+const playAgainYesButton = document.createElement("button");
+playAgainYesButton.innerText = "Yes";
+playAgainYesButton.addEventListener("click", reset);
+
+const playAgainNoButton = document.createElement("button");
+playAgainNoButton.innerText = "No";
+playAgainNoButton.addEventListener("click", reset);
 
 function getHumanChoice(choice) {
   humanResultContainer.textContent = `You picked ${choice}`;
@@ -44,9 +55,15 @@ function getComputerChoice() {
 
 function playGame() {
   if (playerScore === 5) {
-    gameResultContainer.innerText = "You win the game!";
+    gameResultContainer.innerText += "\n You win the game!";
+    gameResultContainer.innerText += "\n \n \n Play again?";
+    gameResultContainer.appendChild(playAgainYesButton);
+    gameResultContainer.appendChild(playAgainNoButton);
   } else if (computerScore === 5) {
-    gameResultContainer.innerText = "You lose the game!"; 
+    gameResultContainer.innerText += "\n You lose the game!"; 
+    gameResultContainer.innerText += "\n \n \n Play again?";
+    gameResultContainer.appendChild(playAgainYesButton);
+    gameResultContainer.appendChild(playAgainNoButton);
   }
 }
 
@@ -54,15 +71,24 @@ function playGame() {
 function playRound(humanChoice, computerChoice) {
   if (humanChoice.toLowerCase() === computerChoice) {
     gameResultContainer.innerText = "Draw! No points awarded!"
+    humanResultContainer.innerText += `\n Human Score: ${playerScore}`;
+    computerResultContainer.innerText += `\n Computer Score: ${computerScore} `;
   } else if (humanChoice === "Rock" && computerChoice === "scissors" || humanChoice === "Paper" && computerChoice === "rock" || humanChoice === "Scissors" && computerChoice === "paper") {
     playerScore++;
     gameResultContainer.innerText = `You win this round! ${humanChoice} beats ${computerChoice}`;
+    humanResultContainer.innerText += `\n Human Score: ${playerScore}`;
+    computerResultContainer.innerText += `\n Computer Score: ${computerScore} `;
   } else if (humanChoice === "Scissors" && computerChoice === "rock" || humanChoice === "Rock" && computerChoice === "paper" || humanChoice === "Paper" && computerChoice === "scissors") {
     computerScore++;
-    gameResultContainer.innerText = `You lose this round! ${computerChoice} beats ${humanChoice}`
+    gameResultContainer.innerText = `You lose this round! ${computerChoice} beats ${humanChoice}`;
+    humanResultContainer.innerText += `/n Human Score: ${playerScore}`;
+    computerResultContainer.innerText += `/n Computer Score: ${computerScore} `;
   } else {
     gameResultContainer.innerText = "no";
   }
   playGame();
 }
 
+function reset() {
+  
+}
